8c3aa6e89357703bcad2c888fc5dabbd4b548577
titan676-cmd <mandark676@gmail.com>
2025-12-31 17:22:48 +0100

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,72 @@
+import socket 
+import optparse
+import threading
+
+def logo_co(toll):
+    print("")
+    print(f"\033[1;37m████████╗██╗████████╗░█████╗░███╗░░██╗")
+    print(f"\033[1;37m╚══██╔══╝██║╚══██╔══╝██╔══██╗████╗░██║")
+    print(f"\033[1;37m░░░██║░░░██║░░░██║░░░███████║██╔██╗██║")
+    print(f"\033[1;37m░░░██║░░░██║░░░██║░░░██╔══██║██║╚████║")
+    print(f"\033[1;37m░░░██║░░░██║░░░██║░░░██║░░██║██║░╚███║")
+    print(f"\033[1;37m░░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝")
+    print("---------------------------------------")
+    print(f"      Welcome to {toll}\n\n")
+
+
+logo_co("Ports Scanner Tool")
+
+class PortScanner:
+    
+    def get_user_inputs(self):
+        object_parser = optparse.OptionParser()
+        object_parser.add_option("--ip",dest="ip_target",help="Enter ip target scan")
+        object_parser.add_option("--range1",dest="range1",help="Enter the start port")
+        object_parser.add_option("--range2",dest="range2",help="Enter the end port")
+        (user_input, argument) = object_parser.parse_args()
+
+        if not user_input.ip_target:
+            object_parser.error("[-] Specify an ip target please")
+        elif not user_input.range1:
+            object_parser.error("[-] Specify an range1 please")
+        elif not user_input.range2:
+            object_parser.error("[-] Specify an range2 please")
+        
+        return user_input
+
+
+    def scan_single_port(self,ip, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.3)
+        self.connect = sock.connect_ex((ip, port))
+        if self.connect == 0:
+            try:
+                service = socket.getservbyport(port)
+            except OSError:
+                service = "Unknown"
+
+            print(f"[+] port: \033[1;33m{port} is open", "service: {service}")
+
+            sock.close()
+
+    def Threads_scan(self, ip, start, end):
+        threads = []
+        print("\n[+] port scanner started!")
+        print("---------------------------------------")
+        for port in range(start, end +1):
+            t = threading.Thread(target=self.scan_single_port, args=(ip, port))
+            t.start()
+            threads.append(t)
+
+        # "استنى كل الـ Threads اللي شغّالة تخلص شغلها قبل ما تكمل البرنامج"
+        for t in threads:
+            t.join() 
+        if self.connect != 0:
+            print("[-] not found port")
+
+object1 = PortScanner()
+user_input = object1.get_user_inputs()
+
+start = int(user_input.range1)
+end = int(user_input.range2)
+object1.Threads_scan(user_input.ip_target, start, end)
